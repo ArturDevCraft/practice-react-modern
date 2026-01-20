f1b686581e1b5a965ddd0d5cbba0e0d9ba145c26
Artur <artur.czerwiak@gmail.com>
2026-01-20 22:22:33 +0100

    Task 05 - create validation

--- 05/ContactForm.js
+++ 05/ContactForm.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 
 import account from './account';
 
@@ -24,11 +24,54 @@ function ContactForm() {
     ];
     // eslint-disable-next-line no-console
     console.log(account);
-    const init = Object.fromEntries(formFields.map((field) => [field.name, '']));
-    const reducer = (state, { name, value }) => ({ ...state, [name]: value });
-    const [state, dispatch] = useReducer(reducer, init);
+    const initFormVal = Object.fromEntries(formFields.map((field) => [field.name, '']));
+    const reducerForm = (state, { name, value }) => ({ ...state, [name]: value });
+    const [formValues, formDispatch] = useReducer(reducerForm, initFormVal);
+    const [formErrors, setFormErrors] = useState(null);
+
+    function checkPattern(pattern, val) {
+        const regex = new RegExp(pattern);
+        return regex.test(val);
+    }
+
+    function isNotEmpty(string = null) {
+        if (string === null || string.trim() === '') {
+            return false;
+        }
+        return true;
+    }
+
+    function validate(e, fields) {
+        e.preventDefault();
+        const errors = new Map([]);
+
+        fields.forEach((field) => {
+            const { value } = e.target[field.name];
+            const { label } = field;
+            if (field.required && !isNotEmpty(value)) {
+                errors.set(field.name, `Pole: ${label} jest puste`);
+            } else if (field.regex && isNotEmpty(value)) {
+                const isCorrect = checkPattern(field.regex, value);
+                if (!isCorrect) errors.set(field.name, `Pole: ${label} ma niewłaściwy format`);
+            }
+        });
+
+        if (errors.size > 0) {
+            return errors;
+        }
+        return null;
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const errors = validate(e, formFields);
+        if (errors !== null) {
+            setFormErrors(errors);
+        }
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             {formFields.map((field) => {
                 const Tag = field.type === 'textarea' ? 'textarea' : 'input';
                 return (
@@ -38,10 +81,13 @@ function ContactForm() {
                             id={field.name}
                             name={field.name}
                             type={field.type}
-                            required={field.required}
-                            value={state[field.name]}
-                            onChange={(e) => dispatch(e.target)}
+                            // required={field.required}
+                            value={formValues[field.name]}
+                            onChange={(e) => formDispatch(e.target)}
                         />
+                        {formErrors && formErrors.has(field.name) && (
+                            <span>{formErrors.get(field.name)}</span>
+                        )}
                     </div>
                 );
             })}
